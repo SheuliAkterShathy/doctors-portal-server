37c17aa1703a5b2c53f36b338dfdc52ecdbef185
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-11-19 23:21:38 +0600

    verifyAdmin add

--- index.js
+++ index.js
@@ -39,6 +39,22 @@ async function run(){
        const appointmentOptionCollection=client.db('doctorsPortal').collection('appointmentOptions');
        const bookingsCollection=client.db('doctorsPortal').collection('bookings');
        const usersCollection=client.db('doctorsPortal').collection('users');
+       const doctorsCollection=client.db('doctorsPortal').collection('doctors');
+
+      //   NOTE: make sure use verifyAdmin after verifyJWT
+
+      const verifyAdmin = async(req, res,next) =>{
+         // console.log('inside verify admin', req.decoded.email)
+         const decodedEmail = req.decoded.email;
+         const query = {email: decodedEmail};
+         const user = await usersCollection.findOne(query);
+
+         if(user?.role !== 'admin'){
+          return res.status(403).send({message: 'forbidden access'})
+         }
+         next()
+
+      }
 
 
        app.get('/appointmentOptions', async(req,res) =>{
@@ -102,6 +118,13 @@ async function run(){
             }
          ]).toArray();
          res.send(options)
+       });
+
+
+       app.get('/appointmentSpecialty', async(req,res) =>{
+         const query = {}
+         const result = await appointmentOptionCollection.find(query).project({name: 1}).toArray();
+         res.send(result)
        })
 
 
@@ -166,16 +189,7 @@ async function run(){
          res.send(result);
        });
 
-       app.put('/users/admin/:id',verifyJWT, async(req,res) =>{
-         const decodedEmail = req.decoded.email;
-         const query = {email: decodedEmail};
-         const user = await usersCollection.findOne(query);
-
-         if(user?.role !== 'admin'){
-          return res.status(403).send({message: 'forbidden access'})
-         }
-
-
+       app.put('/users/admin/:id',verifyJWT, verifyAdmin, async(req,res) =>{
          const id = req.params.id;
          const filter = {_id: ObjectId(id)}
          const options = {upsert: true};
@@ -187,6 +201,25 @@ async function run(){
          const result = await usersCollection.updateOne(filter,updateDoc,options);
          res.send(result);
 
+       });
+
+       app.get('/doctors',verifyJWT,verifyAdmin, async(req,res) =>{
+         const query = {};
+         const doctors = await doctorsCollection.find(query).toArray();
+         res.send(doctors)
+       })
+
+       app.post('/doctors',verifyJWT,verifyAdmin, async(req,res) =>{
+         const doctor = req.body;
+         const result = await doctorsCollection.insertOne(doctor);
+         res.send(result);
+       });
+
+       app.delete('/doctors/:id', verifyJWT,verifyAdmin, async(req,res) =>{
+         const id = req.params.id;
+         const filter = {_id: ObjectId(id)};
+         const result = await doctorsCollection.deleteOne(filter);
+         res.send(result);
        })
 
     }
